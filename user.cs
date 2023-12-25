f302f759636a3e71a0a54b3792da3b4cd93b083f
iKnowiTim <51918974+iKnowiTim@users.noreply.github.com>
2023-12-25 23:26:52 +0300

    modify user for json format

--- user.cs
+++ user.cs
@@ -1,12 +1,30 @@
-class User
+using System.Text.Json.Serialization;
+
+public class User
 {
-    public User(string login, string password, int balance)
+    public User(string sessionId, string login, string password, int balance)
     {
+        SessionId = sessionId;
         Login = login;
         Password = password;
         Balance = balance;
     }
+    [JsonPropertyName("sessionId")]
+    public string SessionId { get; set; }
+    [JsonPropertyName("login")]
     public string Login { get; set; }
+    [JsonPropertyName("password")]
     public string Password { get; set; }
+    [JsonPropertyName("balance")]
     public int Balance { get; set; }
+
+
+    public int? GetBalance(string login, string password)
+    {
+        if (Login == login && Password == password)
+        {
+            return Balance;
+        }
+        else return null;
+    }
 }
